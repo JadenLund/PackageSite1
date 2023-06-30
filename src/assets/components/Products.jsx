553ef10309e4e0c557ee8a68a441150c4840f9d3
JadenLund <lundjaden@gmail.com>
2023-06-29 23:42:19 -0700

    adding in height for image slides

--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -6,14 +6,15 @@ export default function Products() {
   return (
     <div className="h-screen">
       {information.options.map((info) => (
-        <div>
+        <div className="">
+          {/* maybe put shit here */}
           {info.display == undefined ? (
             ""
           ) : (
             <div>
               <Card>
                 {info.display.map((slides) => (
-                  <img src={slides.image} />
+                  <img className="h-z" src={slides.image} />
                 ))}
               </Card>
             </div>
